6718661a8f7f23da90ed4ff074a24cf2eaf76826
Canfin Obrayan <104076031+Obrayancanfin@users.noreply.github.com>
2024-09-18 16:53:45 +0200

    Add files via upload

--- src/main/java/org/example/game_tournement/Entity/Tournament.java
+++ src/main/java/org/example/game_tournement/Entity/Tournament.java
@@ -1,6 +1,9 @@
 package org.example.game_tournement.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,10 +22,17 @@ public class Tournament {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Ce champ doit être rempli !")
     private String name;
+    @NotBlank(message = "Sélectionnez une valeur")
     private String game;
+    @NotBlank(message = "Sélectionnez une valeur")
     private String format;
+    @NotBlank(message = "Ce champ doit être rempli !")
     private String rules;
+    @Min(value = 2, message = "Le nombre de joueurs doit être au moins 2")
     private int numberLimitePlayer;
+
+    @NotNull(message = "La date est obligatoire !")
     private LocalDate dateTournament;
 }
--- src/main/java/org/example/game_tournement/Entity/User.java
+++ src/main/java/org/example/game_tournement/Entity/User.java
@@ -2,8 +2,8 @@ package org.example.game_tournement.Entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
@@ -18,33 +18,18 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotNull(message="Username is a required field.")
-    @Size(min = 1, max = 32, message="Username must be between 1 and 32 characters.")
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire") 
     private String username;
 
-
-    @NotBlank(message = "Le mot de passe est obligatoire")
-    @Length(min = 8, max = 40, message = "Le mot de passe doit contenir entre 8 et 40 caractères")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!.%*?&])[A-Za-z\\d@$!%*.?&]{8,40}$", message = "Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial")
-
+    @NotBlank(message = "Le mot de passe est obligatoire") @Length(min = 8, max = 40, message = "Le mot de passe doit contenir entre 8 et 40 caractères") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!.%*?&])[A-Za-z\\d@$!%*.?&]{8,40}$", message = "Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial")
     private String password;
 
-    @Transient
-    private String repeatedPassword;
-
-    @NotBlank(message = "L ' email est obligatoire")
-    @Length(min = 8, max = 40, message = "l'email doit contenir au moins 6 caractéres")
-    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "L'email doit etre du  format *@*.*")
-
-    private String email;
-
-    @Transient
-    private String repeatedEmail;
+    @Transient private String repeatedPassword; // @Transient --> non-persistence en BDD
+    private String avatarPath; // En partant du principe que l'on stockera les avatars hors BDD pour l'instant (en local dans l'appli)
 
     @Builder.Default
     private String roles = "USER";
 
-
-    private String avatarPath; // En partant du principe que l'on stockera les avatars hors BDD pour l'instant (en local dans l'appli)
-
+    
 }
+
