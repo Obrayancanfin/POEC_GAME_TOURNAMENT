1caeeb094090e4248fe0c9bb4902e0e3d89655a0
Canfin Obrayan <104076031+Obrayancanfin@users.noreply.github.com>
2024-09-18 11:14:02 +0200

    Update User.java

--- src/main/java/org/example/game_tournement/Entity/User.java
+++ src/main/java/org/example/game_tournement/Entity/User.java
@@ -18,8 +18,10 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotBlank(message = "Le nom d'utilisateur est obligatoire") private String username;
-    @NotBlank(message = "Le mot de passe est obligatoire") @Length(min = 8, max = 40, message = "Le mot de passe doit contenir entre 8 et 40 caractères") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!.%*?&])[A-Za-z\\d@$!%*?&]{8,40}$", message = "Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial") private String password;
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!.%*?&])[A-Za-z\\d@$!%*.?&]{8,40}$"
+    private String username;
+    @NotBlank(message = "Le mot de passe est obligatoire") @Length(min = 8, max = 40, message = "Le mot de passe doit contenir entre 8 et 40 caractères") @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!.%*?&])[A-Za-z\\d@$!%*.?&]{8,40}$", message = "Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial") p
+    private String password;
     @Transient private String repeatedPassword; // @Transient --> non-persistence en BDD
     private String avatarPath; // En partant du principe que l'on stockera les avatars hors BDD pour l'instant (en local dans l'appli)
 
